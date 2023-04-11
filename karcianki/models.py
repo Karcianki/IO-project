39ae8e0ff7f5da8d394f58439d0d2aced42ed919
piter12345 <piotrek.blinowski@gmail.com>
2023-04-11 14:28:46 +0200

    models.py

--- karcianki/models.py
+++ karcianki/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+
+
+class Game(models.Model):
+    game_id = models.IntegerField()
+
+    def __str__(self):
+        return self.game_id
+
+class Player(models.Model):
+    player_id = models.IntegerField()
+    nickname = models.CharField(max_length=10)
+    game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nickname
