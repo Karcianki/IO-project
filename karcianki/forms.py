ea48f20ae15b8e957f197b8efa1d3c5889341ba3
piter12345 <piotrek.blinowski@gmail.com>
2023-04-11 18:35:11 +0200

    merge z baza_danych

--- karcianki/forms.py
+++ karcianki/forms.py
@@ -2,7 +2,21 @@ from django import forms
 
 from django.core.exceptions import ValidationError
 
-class PokerHostForm(forms.Form):
+class AForm(forms.Form):
+    nickname = forms.CharField(min_length=4, max_length=10, 
+                               required=True,
+                               widget=forms.TextInput(
+                                attrs={'placeholder': 'Nick gracza'}
+                               ),
+                               help_text="Podaj swój nick")
+    
+    def clean_nickname(self):
+        data =  self.cleaned_data['nickname']
+        if not all(c.islower() for c in data):
+            raise ValidationError('Nick nie składa się z samych małych liter')
+        return data
+
+class PokerHostForm(AForm):
     n_players = forms.DecimalField(min_value=2, max_value=10, 
                                    required=True, 
                                    widget=forms.NumberInput(
@@ -15,67 +29,22 @@ class PokerHostForm(forms.Form):
                                             attrs={'placeholder': 100}
                                           ),
                                           help_text="Podaj liczbę żetonów na gracza")
-    nickname = forms.CharField(min_length=4, max_length=10, 
-                               required=True,
-                               widget=forms.TextInput(
-                                attrs={'placeholder': 'Nick gracza'}
-                               ),
-                               help_text="Podaj swój nick")
     
-    def clean_nickname(self):
-        data =  self.cleaned_data['nickname']
-        if not all(c.islower() for c in data):
-            raise ValidationError('Nick nie składa się z samych małych liter')
-        return data
-    
-class TysiacHostForm(forms.Form):
+class TysiacHostForm(AForm):
     n_players = forms.DecimalField(min_value=2, max_value=4, 
                                    required=True, 
                                    widget=forms.NumberInput(
                                     attrs={'placeholder': 2}
                                    ),
                                    help_text="Podaj liczbę graczy (od 2 do 4)")
-    nickname = forms.CharField(min_length=4, max_length=10, 
-                               required=True,
-                               widget=forms.TextInput(
-                                attrs={'placeholder': 'Nick gracza'}
-                               ),
-                               help_text="Podaj swój nick")
-    def clean_nickname(self):
-        data =  self.cleaned_data['nickname']
-        if not all(c.islower() for c in data):
-            raise ValidationError('Nick nie składa się z samych małych liter')
-        return data
     
-class BrydzHostForm(forms.Form):
-    nickname = forms.CharField(min_length=4, max_length=10, 
-                               required=True,
-                               widget=forms.TextInput(
-                                attrs={'placeholder': 'Nick gracza'}
-                               ),
-                               help_text="Podaj swój nick") 
-    def clean_nickname(self):
-        data =  self.cleaned_data['nickname']
-        if not all(c.islower() for c in data):
-            raise ValidationError('Nick nie składa się z samych małych liter')
-        return data
+class BrydzHostForm(AForm):
+    pass
 
-class PlayerForm(forms.Form):
-    #nie zmieniałem bo zaczęła się debata na grupie
+class PlayerForm(AForm):
     board_number = forms.DecimalField(min_value=2, max_value=4, 
                                    required=True, 
                                    widget=forms.NumberInput(
                                     attrs={'placeholder': 2}
                                    ),
                                    help_text="Podaj sześciocyfrowy numer gry")
-    nickname = forms.CharField(min_length=4, max_length=10, 
-                               required=True,
-                               widget=forms.TextInput(
-                                attrs={'placeholder': 'Nick gracza'}
-                               ),
-                               help_text="Podaj swój nick") 
-    def clean_nickname(self):
-        data =  self.cleaned_data['nickname']
-        if not all(c.islower() for c in data):
-            raise ValidationError('Nick nie składa się z samych małych liter')
-        return data
--- karcianki/views.py
+++ karcianki/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from karcianki.forms import PokerHostForm
+from karcianki.forms import PokerHostForm, TysiacHostForm, BrydzHostForm, PlayerForm
+
 from karcianki.models import Game, Player
 
 def index(request):
@@ -39,3 +40,71 @@ def poker(request):
         'game_id': game_id
     }
     return render(request, 'poker.html', context)
+
+def tysiacHost(request):
+    if request.method == 'POST':
+        form = TysiacHostForm(request.POST)
+
+        if form.is_valid():
+            return HttpResponseRedirect(reverse('tysiacHost'))
+    else:
+        form = TysiacHostForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'logowanie/tysiac/host.html', context)
+
+def brydzHost(request):
+    if request.method == 'POST':
+        form = BrydzHostForm(request.POST)
+
+        if form.is_valid():
+             return HttpResponseRedirect(reverse('brydzHost'))
+    else:
+        form = BrydzHostForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'logowanie/brydz/host.html', context)
+
+def pokerPlayer(request):
+    if request.method == 'POST':
+        form = PlayerForm(request.POST)
+
+        if form.is_valid():
+            return HttpResponseRedirect(reverse('pokerPlayer'))
+    else:
+        form = PlayerForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'logowanie/poker/login.html', context)
+def tysiacPlayer(request):
+    if request.method == 'POST':
+        form = PlayerForm(request.POST)
+
+        if form.is_valid():
+            return HttpResponseRedirect(reverse('tysiacPlayer'))
+    else:
+        form = PlayerForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'logowanie/tysiac/login.html', context)
+def brydzPlayer(request):
+    if request.method == 'POST':
+        form = PlayerForm(request.POST)
+
+        if form.is_valid():
+            return HttpResponseRedirect(reverse('brydzPlayer'))
+    else:
+        form = PlayerForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'logowanie/brydz/login.html', context)
