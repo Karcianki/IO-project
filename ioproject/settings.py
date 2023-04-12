2af12a702a1f2e8b0fd5a695a4e8c5529881048b
piter12345 <piotrek.blinowski@gmail.com>
2023-04-12 23:02:18 +0200

    config pytest

--- ioproject/settings.py
+++ ioproject/settings.py
@@ -125,10 +125,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-NOSE_ARGS = [
-    '--with-coverage',
-    '--cover-package=karcianki'
-]
