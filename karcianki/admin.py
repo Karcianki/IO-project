fb00c365ec48789a4fb37983240a2f6c7d50fb3b
piter12345 <piotrek.blinowski@gmail.com>
2023-04-12 12:00:36 +0200

    poprawka do pylint

--- karcianki/admin.py
+++ karcianki/admin.py
@@ -1,6 +1,7 @@
+"""Django admin module."""
 from django.contrib import admin
 
-from .models import Player, Game
+from .models import Game, Player
 
-admin.site.register(Player)
 admin.site.register(Game)
+admin.site.register(Player)
--- karcianki/urls.py
+++ karcianki/urls.py
@@ -1,9 +1,6 @@
 """ioproject URL Configuration"""
 
-from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 
 from . import views
 
