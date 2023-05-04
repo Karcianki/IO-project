70ffa4c269a3b0f0c945d3120f11327470d002e8
Maciek <mmioduszewski4@gmail.com>
2023-05-04 22:36:25 +0200

    added_react

--- ioproject/settings.py
+++ ioproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'ioproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'ioproject' / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'reactapp/build'), BASE_DIR / 'ioproject' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +127,10 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'reactapp/build/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
