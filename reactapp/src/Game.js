86bdfaaf66aea32ed99cbeb609a18c8e5b20186e
piter12345 <piotrek.blinowski@gmail.com>
2023-05-05 18:29:28 +0200

    Game.js

--- reactapp/src/Game.js
+++ reactapp/src/Game.js
@@ -1,7 +1,7 @@
 import React, { Component, useState } from "react";
 import "./static/styles/poker.css";
 import table from './static/images/stol.png';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams, useLocation } from 'react-router-dom';
 
 class Player extends Component {
     render() {
@@ -16,88 +16,68 @@ class Player extends Component {
     }
 }
 
+const Game = () => {
+    const [showRules, setShowRules] = useState(false);
 
-class Game extends Component {
-    state = {showRules: false};
+    const [searchParams] = useSearchParams();
+    const game_id = searchParams.get('game_id');
 
-    toggleRules = () => {
-        this.setState({ showRules: !this.state.showRules });
+    const toggleRules = () => {
+        setShowRules(!showRules);
     };
 
-    GameHeader = () => {
-        return (
+    return (
+        <div>
             <header>
                 <div>
-                    {this.props.name}
+                    Poker
                 </div>
                 <div>
-                    Numer gry: { this.props.game_id }
+                    Numer gry {game_id}
                 </div>
-                <button onClick={this.toggleRules} type="submit" aria-label="info"><span className="fa-solid fa-question"></span></button>
+                <button onClick={toggleRules} type="submit" aria-label="info"><span className="fa-solid fa-question"></span></button>
             </header>
-        )
-    };
 
-    GameRules = () => {
-        return (
-            <div className={this.state.showRules? "zasady show" : "zasady"}>
-                Zasady
-            </div>
-        )
-    }
+            <div className="page">
 
-    GameBoard = () => {
-        return (
-            <div className="plansza" id="game_board" game_id={this.props.game_id}>
-                <div className="rzad">
-                    <Player id="gracz1" name="chuj" /> 
-                    <Player id="gracz4" name="siur" />
-                    <Player id="gracz6" name="parówa" />
-                    <Player id="gracz8" name="kutas" />
+                <div className="plansza" id="game_board" game_id={game_id}>
+                    <div className="rzad">
+                        <Player id="gracz1" name="chuj" /> 
+                        <Player id="gracz4" name="siur" />
+                        <Player id="gracz6" name="parówa" />
+                        <Player id="gracz8" name="kutas" />
+                    </div>
+                    <div className="rzad" id="ze_stolem">
+                        <Player id="gracz0" name="fiut" />
+                        <img src={table} alt="" className="stol"/>
+                        <Player id="gracz1" name="idk" />
+                    </div>
+                    <div className="rzad">
+                        <Player id="gracz3" name="idk" />
+                        <Player id="gracz5" name="idk" />
+                        <Player id="gracz7" name="idk" />
+                        <Player id="gracz9" name="idk" />
+                    </div>
                 </div>
-                <div className="rzad" id="ze_stolem">
-                    <Player id="gracz0" name="fiut" />
-                    <img src={table} alt="" className="stol"/>
-                    <Player id="gracz1" name="idk" />
-                </div>
-                <div className="rzad">
-                    <Player id="gracz3" name="idk" />
-                    <Player id="gracz5" name="idk" />
-                    <Player id="gracz7" name="idk" />
-                    <Player id="gracz9" name="idk" />
-                </div>
-            </div>
-        )
-    }
 
-    GameOptions = () => {
-        return (
-            <div className="opcje">
-                <button type="submit">Pass</button>
-                <button type="submit">Sprawdź</button>
-                <form>
-                    <input type="number" step="5" className="licytuj" min="0" max="10000"/>
-                    <button type="submit">Postaw</button>
-                </form>
-                <Link to='../'>
-                    <button type="submit" id="quit">Wyjdź</button>
-                </Link>
-            </div>
-        )
-    }
+                <div className={showRules? "zasady show" : "zasady"}>
+                    Zasady
+                </div>
 
-    render() {
-        return (
-            <div>
-                {this.GameHeader()}
-                <div className="page">
-                    {this.GameBoard()}
-                    {this.GameRules()}
-                    {this.GameOptions()}
+                <div className="opcje">
+                    <button type="submit">Pass</button>
+                    <button type="submit">Sprawdź</button>
+                    <form>
+                        <input type="number" step="5" className="licytuj" min="0" max="10000"/>
+                        <button type="submit">Postaw</button>
+                    </form>
+                    <Link to='../'>
+                        <button type="submit" id="quit">Wyjdź</button>
+                    </Link>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Game;
