83ead847b7ca09619d3d9ce4c5a9753ce052e13c
Weronika Piecuch <we.piecuch@student.uw.edu.pl>
2023-05-07 20:05:09 +0200

    Index test

---
+++ reactapp/tests/index.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { render, act } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../src/App';
+import Login from '../src/Login.js';
+import Host from '../src/Host';
+import Game from '../src/Game';
+import fetchMock from 'jest-fetch-mock';
+
+fetchMock.mockResponse(JSON.stringify({ data: 'mocked data' }));
+
+
+
+jest.mock('../src/static/styles/style.css', () => ({}));
+jest.mock('../src/static/styles/login.css', () => ({}));
+jest.mock('../src/static/styles/poker.css', () => ({}));
+jest.mock('../src/static/images/stol.png', ()=>({}));
+
+describe('App', () => {
+    test('renders App component', () => {
+        render(
+          <App />
+      );
+    });
+  });
+  
+  describe('Login', () => {
+    test('renders Login component', () => {
+       render(
+                <Login />
+            );
+     });
+  });
+  
+  describe('Host', () => {
+    test('renders Host component', () => {
+      render(
+          <Host />
+      );
+    });
+  });
+  
+  describe('Game', () => {
+    test('renders Game component', () => {
+      render(
+        <BrowserRouter>
+          <Game name="POKER" />
+        </BrowserRouter>
+      );
+    });
+  });
