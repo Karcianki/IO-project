5fed56351aa7870346d3301a58d52d243f27c049
Sobczakus1658 <103337067+Sobczakus1658@users.noreply.github.com>
2023-05-07 19:47:42 +0200

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,20 +1,20 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import { BrowserRouter } from 'react-router-dom';
-// import App from '../reactapp/src/App';
+ import React from 'react';
+ import { render, screen } from '@testing-library/react';
+ import { BrowserRouter } from 'react-router-dom';
+ import App from '../reactapp/src/App';
 
 
 
-// describe('Routing', () => {
-//   test('renders App component when path is empty', () => {
-//     render(
-//         <BrowserRouter>
-//           <App />
-//       </BrowserRouter>
-//     );
-//     const appElement = screen.getByRole('heading', { name: 'App' });
-//     expect(appElement).toBeInTheDocument();
-//   });
+ describe('Routing', () => {
+   test('renders App component when path is empty', () => {
+     render(
+         <BrowserRouter>
+           <App />
+       </BrowserRouter>
+     );
+     const appElement = screen.getByRole('heading', { name: 'App' });
+     expect(appElement).toBeInTheDocument();
+   });
 
 
-// });
+ });
