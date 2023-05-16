8593151a567d9591018a7c896d88045cb86d35c5
piter12345 <piotrek.blinowski@gmail.com>
2023-05-16 16:10:35 +0200

    export DATABASE_URL

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-fetch(`http://localhost:8000/api/karcianki/create/`, {
+fetch(`${process.env.DATABASE_URL}/api/karcianki/create/`, {
     method: "POST",
     headers: {
         "Content-Type": "application/json",
