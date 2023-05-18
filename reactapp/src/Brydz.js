253e50b551c50796e5b9d183d15c937a9afd4c3d
Weronika Piecuch <we.piecuch@student.uw.edu.pl>
2023-05-18 13:22:14 +0200

    brydz bez ostrzeżeń

--- reactapp/src/Brydz.js
+++ reactapp/src/Brydz.js
@@ -1,26 +1,8 @@
 //przygotowane pod brydza
 
 
-import React, { Component, useEffect, useState } from "react";
-import "./static/styles/brydz.css";
-import table from './static/images/stol.png';
-import {RulesBrydz} from './Rules'
-import { Link, useSearchParams} from 'react-router-dom';
+import React from "react";
 
-class TPlayer extends Component {
-    render() {
-        return (
-            <div className={ JSON.parse(this.props.data).class } id={this.props.id}>
-                <span className="fa-solid fa-circle-user ikona"></span>
-                <div className="dane">
-                    <div>{ JSON.parse(this.props.data).info }</div>
-                    <div>{ JSON.parse(this.props.data).nickname }</div>
-                    <div>{ JSON.parse(this.props.data).points }</div>
-                </div>
-            </div>
-        )
-    }
-}
 const GameBrydz = () => {
     return (
         <div>
